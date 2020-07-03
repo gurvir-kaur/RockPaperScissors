b955df852b8fcae326271d324fe862ae91ab5668
gurvir-kaur <geer97.gkk@gmail.com>
2020-07-03 20:16:03 +0200

    almost done

--- js/clsRockPaperScissors.js
+++ js/clsRockPaperScissors.js
@@ -3,6 +3,9 @@ class RockPaperScissors {
 
         this.Doc = pDoc;
         this.win = pWindow;
+        this.userChoice = '';
+        this.computerChoice = this.getComputerChoice();
+        this.score = 0;
 
     }
 
@@ -12,5 +15,68 @@ class RockPaperScissors {
         }
     }
 
+    userOption(pChoice) {
+        if (pChoice == 'rock') {
+            this.userChoice = 'rock';
+        } else {
+            if (pChoice == 'paper') {
+                this.userChoice = 'paper';
+            } else {
+                this.userChoice = 'scissors';
+            }
+        }
+
+        this.determineWinner();
+    }
+
+    getComputerChoice() {
+        var choice = Math.floor(Math.random() * 3);
+        switch (choice) {
+            case 0:
+                return 'rock';
+            case 1:
+                return 'paper';
+            case 2:
+                return 'scissors';
+        }
+    }
+
+    determineWinner() {
+        console.log(this.userChoice, this.computerChoice);
+        if (this.userChoice === this.computerChoice) {
+            this.SetScreenValue('This game is a tie!');
+        }
+        if (this.userChoice === 'rock') {
+            if (this.computerChoice === 'paper') {
+                this.SetScreenValue('Computer won.');
+            } else {
+                ++this.score;
+                this.SetScreenValue('You won.');
+            }
+        }
+        if (this.userChoice === 'paper') {
+            if (this.computerChoice === 'rock') {
+                ++this.score;
+                this.SetScreenValue('You won.');
+            } else {
+                this.SetScreenValue('Computer won.');
+            }
+        }
+        if (this.userChoice === 'scissors') {
+            if (this.computerChoice === 'rock') {
+                this.SetScreenValue('Computer won.');
+            } else {
+                ++this.score;
+                this.SetScreenValue('You won.');
+            }
+        }
+    }
+
+    SetScreenValue(pValue) {
+
+        if (pValue != " ") {
+            this.doc.getElementById("value").value = pValue;
+        }
+    }
 
 }
